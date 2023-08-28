2d6ce127865636809734a18568e6b17cfc3106f3
Michael Alimo <michael.alimo@yahoo.com>
2023-08-28 22:54:57 +0000

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,34 +11,21 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-		int i;
+int i, j;
 
-		if (*needle == 0)
-
-			return (haystack);
-
-		while (*haystack)
-		{
-			i = 0;
-
-		if (haystack[i] == needle[i])
-		{
-		do {
-
-		if (needle[i + 1] == '\0')
-
-			return (haystack);
-
-			i++;
-
-		}
-
-		while (haystack[i] == needle[i]);
-
-		}
-
-			haystack++;
-		}
-
-			return ('\0');
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+	for (j = 0; needle[j] != '\0'; j++)
+	{
+	if (haystack[i + j] != needle[j])
+	{
+		break;
+	}
+	}
+	if (needle[j] == '\0')
+	{
+	return (&haystack[i]);
+	}
+	}
+		return (0);
 }
