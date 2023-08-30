e53d355cc901947e8ffb96e49c0598f93fd0847b
Michael Alimo <michael.alimo@yahoo.com>
2023-08-30 23:28:20 +0000

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,8 +9,10 @@
 int _sqrt_recursion(int n)
 {
 		if (n == 0 || n == 1)
+		{
 			return (n);
 			return (_sqrt(0, n));
+		}
 }
 
 /**
@@ -23,8 +25,12 @@ int _sqrt_recursion(int n)
 int _sqrt(int n, int x)
 {
 		if (n > x / 2)
+		{
 			return (-1);
+		}
 		else if (n * n == x)
+		{
 			return (n);
 			return (_sqrt(n + 1, x));
+		}
 }
