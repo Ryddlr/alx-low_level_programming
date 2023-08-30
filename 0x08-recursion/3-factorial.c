d9f436825fcdee4bdd84e934001e2f83a2aa5985
Michael Alimo <michael.alimo@yahoo.com>
2023-08-30 23:04:03 +0000

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,11 +10,11 @@ int factorial(int n)
 {
 		int next;
 
-		if (n == 0)
-			return (1);
-		else if (n < 0)
-			return (-1);
+			if (n == 0)
+				return (1);
+			else if (n < 0)
+				return (-1);
 
 			next = factorial(n - 1);
-			return (n * next);
+				return (n * next);
 }
