be9f2011a07291115df85897cc30242d4a3d9f92
Michael Alimo <michael.alimo@yahoo.com>
2023-08-30 23:14:37 +0000

    updated 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,8 +9,13 @@
 int _pow_recursion(int x, int y)
 {
 		if (y < 0)
+		{
 			return (-1);
+		}
 		else if (y == 0)
+		{
 			return (1);
-			return (_pow_recursion(x, y - 1) * x);
+		}
+		else
+			return (x * _pow_recursion(x, (y - 1)));
 }
