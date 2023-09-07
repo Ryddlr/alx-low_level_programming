9c0f8050ced856186f997481252e8f1076e4199d
Michael Alimo <michael.alimo@yahoo.com>
2023-09-07 01:37:46 +0000

    100-change.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <ctype.h>
-#include <string.h>
 
 /**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array of strings containing the arguments.
- * Return: (0) if successful, (1) if an error occurred.
+ *main - adds positive numbers
+ *@argc: number of arguments
+ *@argv: array of arguments
+ *Return: 0 on success, 1 on failure
  */
+
 int main(int argc, char *argv[])
 {
-	int j = 1;
-	int summ = 0;
+		int i, j, sum = 0;
 
-	if (argc == 1)
-	{
-	printf("0\n");
-	return (0);
-	}
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+		{
+																	printf("Error\n");
+		return (1);
+																	}
+		}
 
-	while (j <= argc - 1)
-	{
-		int i = 0;
+		sum += atoi(argv[i]);
+		}
 
-	while (argv[j][i] != '\0')
-	{
-	if (!isdigit(argv[j][i]))
-	{
-	printf("Error\n");
-	return (1);
-	}															i++;
-	}
-	summ += atoi(argv[j]);
-	j++;
+		printf("%d\n", sum);
 
-	printf("%d\n", summ);
-	return (0);
+		return (0);
 }
